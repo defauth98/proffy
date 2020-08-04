173fa30483c191a65d693fcc1d9bec6b94f806eb
defauth98 <neto.daniribeiro@gmail.com>
2020-08-04 04:42:27 -0300

    Configuração das rotas no web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>teste</h1>
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function TeacherForm() {
+  return <div />;
+}
+
+export default TeacherForm;
