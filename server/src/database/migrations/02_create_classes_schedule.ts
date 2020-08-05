ea735d288f0cc12705648ebe20b555d99ee51b0d
defauth98 <neto.daniribeiro@gmail.com>
2020-08-05 13:03:05 -0300

    Criação das aulas no backend

---
+++ server/src/database/migrations/02_create_classes_schedule.ts
@@ -0,0 +1,26 @@
+import Knex from 'knex'
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('class_schedule', table => {
+    table.increments('id').primary()
+
+    table.integer('week_day').notNullable()
+    table.integer('from').notNullable()
+    table.integer('to').notNullable()
+
+    table.integer('class_id')
+      .notNullable()
+      .references('id')
+      .inTable('classes')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+
+    table.timestamp('created_at')
+      .defaultTo('now()')
+      .notNullable()
+  })
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('class_schedule');
+}
---
+++ server/src/database/migrations/03_connectios.ts
@@ -0,0 +1,18 @@
+import Knex from 'knex'
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('connections', table => {
+    table.increments('id').primary()
+
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('connections');
+}
