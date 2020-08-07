f40160b723d50eff3325e12cb9defc6cbe4f75da
defauth98 <neto.daniribeiro@gmail.com>
2020-08-07 16:53:42 -0300

    Listagem de aulas

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
-import db from '../database/connection';
-import covertHourToMinutes from '../utils/convertHourToMinutes';
+import db from '../database/connection'
+import covertHourToMinutes from '../utils/convertHourToMinutes'
 
 interface ScheduleItem {
   week_day: number,
@@ -11,13 +11,35 @@ interface ScheduleItem {
 
 export default class ClassesController {
   async index(request: Request, response: Response) {
-    const filters = request.query;
+    const filters = request.query
+
+    const subject = filters.subject as string
+    const week_day = filters.week_day as string
+    const time = filters.time as string
 
     if (!filters.week_day || !filters.subject || !filters.time) {
       return response.status(400).json({
         error: "Missing filters to search classes"
       })
     }
+
+    const timeInMinutes = covertHourToMinutes(time)
+
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ?? ', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ?? ', [timeInMinutes])
+      })
+      .where('classes.subject', "=", subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*'])
+
+    return response.json(classes)
+
   }
 
   async create(request: Request, response: Response) {
