c3b552cb875c6b890a0c902478588b615ef8f470
defauth98 <neto.daniribeiro@gmail.com>
2020-08-17 15:00:40 -0300

    Autenticacao de usuario com token jwt

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import ClassesController from './controllers/ClassesController'
 import ConnectionController from './controllers/ConnectionsController'
+import AuthController from './controllers/AuthController'
 
 const routes = express.Router()
 const classesController = new ClassesController()
 const connectionsController = new ConnectionController()
+const authController = new AuthController()
+
+routes.post('/sign', authController.signin)
+routes.post('/login', authController.login)
 
 routes.get('/classes', classesController.index)
 routes.post('/classes', classesController.create)
