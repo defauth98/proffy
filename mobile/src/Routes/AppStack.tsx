f8bc3b653b2cb909b801085e3ba13986ad85e176
defauth98 <neto.daniribeiro@gmail.com>
2020-09-17 15:21:33 -0300

    Tela de login no mobile

--- mobile/src/Routes/AppStack.tsx
+++ mobile/src/Routes/AppStack.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Landing from '../pages/Landing'
-import GiveClasses from '../pages/GiveClasses'
-import StudyTabs from '../Routes/StudyTabs'
+import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
+import StudyTabs from '../Routes/StudyTabs';
+import Login from '../pages/LoginPage';
+import SignUp from '../pages/SignUp';
 
-const { Navigator, Screen } = createStackNavigator()
+const { Navigator, Screen } = createStackNavigator();
 
 function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="Login" component={Login} />
+        <Screen name="SignUp" component={SignUp} />
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
         <Screen name="StudyTabs" component={StudyTabs} />
       </Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
-export default AppStack
+export default AppStack;
---
+++ mobile/src/pages/LoginPage/styles.ts
@@ -0,0 +1,93 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logoWrapper: {
+    flex: 1,
+    backgroundColor: '#8257e5',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    padding: 10,
+  },
+  backgroundImageWrapper: {
+    resizeMode: 'cover',
+
+    width: 250,
+    height: 250,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoProffy: {
+    width: 150,
+    height: 90,
+  },
+  logoText: {
+    width: 150,
+  },
+
+  formWrapper: {
+    flex: 1,
+
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+
+    backgroundColor: '#E6E6F0',
+  },
+
+  formHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  formHeaderText: {
+    fontFamily: 'Poppins_600SemiBold',
+    color: '#32264D',
+    fontSize: 26,
+  },
+  formHeaderRightButton: {},
+  rightButtonText: {
+    color: '#8257E5',
+  },
+  formInputs: {},
+  inputWrapper: {
+    backgroundColor: '#F0F0F7',
+    height: 60,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#E6E6F0',
+  },
+  formInput: {},
+  formInputPassword: {
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+  formInputEmail: {
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  formFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+    paddingVertical: 30,
+  },
+  remember: {},
+  forget: {},
+  formButton: {
+    backgroundColor: '#04D361',
+    padding: 16,
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  formButtonText: {
+    fontFamily: 'Archivo_400Regular',
+    color: '#fff',
+    fontSize: 24,
+  },
+});
+
+export default styles;
