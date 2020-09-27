5867befc4c7fa3fc3fe32e833de005bd01005c31
defauth98 <neto.daniribeiro@gmail.com>
2020-09-27 19:03:53 -0300

    Estrutura e estilizaço da tela de login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Aside from '../../components/Aside';
 import FormInput from '../../components/FormInput';
 
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
 import './styles.css';
 
 const Login: React.FC = () => {
@@ -11,16 +13,35 @@ const Login: React.FC = () => {
       <Aside />
       <div className="login-form">
         <h2 className="title">Fazer login</h2>
+
         <div className="inputs">
           <FormInput label="E-mail" />
           <FormInput label="Senha" />
         </div>
-        <div className="buttons-wrapper">
-          <div className="checkbox-container">
-            <input type="checkbox" id="remenber" />
-            <label htmlFor="remenber">Lembrar-me</label>
+        <div className="buttons-container">
+          <div className="first-line">
+            <div className="checkbox-container">
+              <input type="checkbox" id="remenber" />
+              <label htmlFor="remenber">Lembrar-me</label>
+            </div>
+            <div className="forget-container">
+              <a href="#">Esqueci minha senha</a>
+            </div>
           </div>
-          Esqueci minha senha
+
+          <div className="second-line">
+            <button type="button">Entrar</button>
+          </div>
+        </div>
+
+        <div className="footer">
+          <div className="signup-container">
+            <span>Não tem conta?</span>
+            <a href="#">Cadastre-se</a>
+          </div>
+          <span>
+            É de graça <img alt="Coração roxo" src={purpleHeartIcon} />
+          </span>
         </div>
       </div>
     </div>
