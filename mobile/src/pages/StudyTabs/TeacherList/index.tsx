2e8ddcb4c29e5067b00a3d256c5d94374e579906
defauth98 <neto.daniribeiro@gmail.com>
2020-10-05 15:54:14 -0300

    Adicionando campos de select nos filtros do mobile

--- mobile/src/pages/StudyTabs/TeacherList/index.tsx
+++ mobile/src/pages/StudyTabs/TeacherList/index.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
-import { View, ScrollView, Text } from 'react-native';
 import {
-  TextInput,
-  BorderlessButton,
-  RectButton,
-} from 'react-native-gesture-handler';
+  View,
+  ScrollView,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+
 import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../../../services/api';
 
 import PageHeader from '../../../components/PageHeader';
 import TeacherItem, { Teacher } from '../../../components/TeacherItem';
+import Select from '../../../components/Select';
 
 import styles from './styles';
-import { useFocusEffect } from '@react-navigation/native';
 
 const TeacherList: React.FC = () => {
   const [isFilterVisible, setFilterVisible] = useState(false);
@@ -60,7 +63,10 @@ const TeacherList: React.FC = () => {
   }
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+    >
       <PageHeader
         title="Proffs disponíveis"
         headerRight={
@@ -70,35 +76,72 @@ const TeacherList: React.FC = () => {
         }
       >
         {isFilterVisible && (
-          <View style={styles.searchForm}>
-            <Text style={styles.label}>Matéria</Text>
-            <TextInput
-              placeholderTextColor="#c1bccc"
-              style={styles.input}
-              placeholder="Qual a matéria?"
-              value={subject}
-              onChangeText={(text) => setSubject(text)}
+          <ScrollView style={styles.searchForm}>
+            <Select
+              label="Matéria"
+              items={[
+                { value: 'Artes', label: 'Artes' },
+                { value: 'Biologia', label: 'Biologia' },
+                { value: 'Matemática', label: 'Matemática' },
+                { value: 'Inglês', label: 'Inglês' },
+                { value: 'Geografia', label: 'Geografia' },
+                { value: 'História', label: 'História' },
+                { value: 'Português', label: 'Português' },
+                { value: 'Química', label: 'Química' },
+                { value: 'Física', label: 'Física' },
+              ]}
+              placeholder="Selecione"
+              defaultValue={subject}
+              onChangeItem={(item) => setSubject(item.value)}
             />
-
             <View style={styles.inputGroup}>
               <View style={styles.inputBlock}>
-                <Text style={styles.label}>Dia da semana</Text>
-                <TextInput
-                  placeholderTextColor="#c1bccc"
-                  style={styles.input}
-                  placeholder="Qual o dia?"
-                  value={week_day}
-                  onChangeText={(text) => setWeekDay(text)}
+                <Select
+                  label="Dia da semana"
+                  items={[
+                    { label: 'Segunda', value: 1 },
+                    { label: 'Terça', value: 2 },
+                    { label: 'Quarta', value: 3 },
+                    { label: 'Quinta', value: 4 },
+                    { label: 'Sexta', value: 5 },
+                  ]}
+                  placeholder="Selecione"
+                  defaultValue={week_day}
+                  onChangeItem={(item) => setWeekDay(item.value)}
                 />
               </View>
+
               <View style={styles.inputBlock}>
-                <Text style={styles.label}>Horário</Text>
-                <TextInput
-                  placeholderTextColor="#c1bccc"
-                  style={styles.input}
-                  placeholder="Qual horário?"
-                  value={time}
-                  onChangeText={(text) => setTime(text)}
+                <Select
+                  label="Horário"
+                  items={[
+                    { label: '00h', value: '00:00' },
+                    { label: '01h', value: '01:00' },
+                    { label: '02h', value: '02:00' },
+                    { label: '03h', value: '03:00' },
+                    { label: '05h', value: '05:00' },
+                    { label: '06h', value: '06:00' },
+                    { label: '07h', value: '07:00' },
+                    { label: '08h', value: '08:00' },
+                    { label: '09h', value: '09:00' },
+                    { label: '10h', value: '10:00' },
+                    { label: '11h', value: '11:00' },
+                    { label: '12h', value: '12:00' },
+                    { label: '13h', value: '13:00' },
+                    { label: '14h', value: '14:00' },
+                    { label: '15h', value: '15:00' },
+                    { label: '16h', value: '16:00' },
+                    { label: '17h', value: '17:00' },
+                    { label: '18h', value: '18:00' },
+                    { label: '19h', value: '19:00' },
+                    { label: '20h', value: '20:00' },
+                    { label: '21h', value: '21:00' },
+                    { label: '22h', value: '22:00' },
+                    { label: '23h', value: '23:00' },
+                  ]}
+                  placeholder="Selecione"
+                  defaultValue={time}
+                  onChangeItem={(item) => setTime(item.value)}
                 />
               </View>
             </View>
@@ -109,7 +152,7 @@ const TeacherList: React.FC = () => {
             >
               <Text style={styles.submitButtonText}>Filtrar</Text>
             </RectButton>
-          </View>
+          </ScrollView>
         )}
       </PageHeader>
       <ScrollView
@@ -129,7 +172,7 @@ const TeacherList: React.FC = () => {
           );
         })}
       </ScrollView>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
