a46e309c02d2cf59e9da03edf34ac43c2e282a73
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-12-04 11:00:26 -0300

    update api.ts file

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const api = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.API_URL || 'https://proffy-deploy-2.herokuapp.com/' ,
 });
 
 export default api;
